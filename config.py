98c42f91ccabf11f188ca093327ad801f781629c
aleksandravolne <115566224+aleksandravolne@users.noreply.github.com>
2022-12-27 17:15:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+correct_name = 'Иван'
+correct_surname = 'Петров'
+correct_email = 'test123@testmail.ru'
+correct_password = 'jvL5TWr9'
+correct_code = '464821'
+
+incorrect_name = '123'
+incorrect_surname = '123'
+incorrect_email = '123'
+incorrect_password = '123'
+incorrect_code = '123456'
+
+incorrect_name_2 = '!@#$%^&*()'
+incorrect_surname_2 = '!@#$%^&*()'
+incorrect_email_2 = '!@#$%^&*()'
+incorrect_password_2 = '!@#$%^&*()'
+
+incorrect_name_3 = 'йцукенqwerty'
+incorrect_surname_3 = 'йцукенqwerty'
+incorrect_email_3 = 'йцукенqwerty'
+incorrect_password_3 = 'йцукенqwerty'
+
+incorrect_password_4 = 'jL5TWr95'
+
+correct_phone = '+79211234567'
